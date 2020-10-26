0b97777cb13a76243c80fb468a2632572700b787
Christopher Williams <christopher876@outlook.com>
2020-10-26 11:57:41 +0000

    Clean up Interpreter Class and file
    - Add Parser to parse the input of what comes out of the reader
    - Add Reset to reset the position and EOF signal
    - Ability to properly read strings
    - Add Shell with the only ability to input numbers and one command "exit"

--- Interpreter.ts
+++ Interpreter.ts
@@ -1,22 +1,13 @@
-import { Functions } from "./Functions";
-
-function delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-}
-
-
-export interface IHash {
-    [details: string] : string;
-}
+import { Parser } from "./Parser";
+import { CSymbol } from "./CSymbol";
 
 export class Interpreter {
     EOF : boolean = false;
     source : string;
     position : number;
     #types : string[];
-
-    globalVariables : IHash = {};
-    localVariables : string[];
+    parser : Parser;
+    symbols : CSymbol;
 
     set Position(newPosition : number){
         if(newPosition <= this.source.length){
@@ -30,14 +21,9 @@ export class Interpreter {
     constructor(source : string, position : number = 0) {
         this.source = source;
         this.position = position;
-        this.localVariables = [];
-        //this.#stack = [];
         this.#types = [];
-    }
-
-    RegisterTypes(){
-        //Defualt types
-        this.#types.push("int","float","string");
+        this.parser = new Parser();
+        this.symbols = new CSymbol();
     }
 
     Step() : string {
@@ -46,10 +32,17 @@ export class Interpreter {
         return next;
     }
 
-    Execute(){
+    Reset(){
+        this.EOF = false;
+        this.position = 0;
+    }
+
+    Execute() : string{
         let next = this.Step();
         let word : string[] = [];
         const line : string[] = [];
+
+        let isString : boolean = false;
 
         while(!this.EOF && next !== "\n"){
             // Skip whitespace
@@ -57,21 +50,85 @@ export class Interpreter {
                 next = this.Step();
             }
 
+            // We are a string
+            if(next === "\""){
+                isString = true;
+                next = this.Step();
+
+                // Get the string
+                while(next !== "\""){
+                    word.push(next);
+                    next = this.Step();
+                }
+
+                next = this.Step();
+                line.push(word.join(""));
+                continue;
+            }
+
             // While we are not whitespace
-            while(!/^\s+$/.test(next) && next !== "\n" && !this.EOF){
+            while((!/^\s+$/.test(next) && next !== "\n" && !this.EOF)){
                 if(word){
                     word.push(next);
                     next = this.Step();
                 }
+
+                // Are we a number
+                if(/^\d+$/.test(word.join(""))){
+                    // Get the entire number (multidigit)
+                    while(true){
+                        if(/^\d+$/.test(next)){
+                            word.push(next);
+                            next = this.Step();
+                        }
+                        else
+                            break;
+                    }
+
+                    line.push(word.join(""));
+                    word = [];
+                    break;
+                }
+
                 else{
                     break;
                 }
             }
 
             if(word.length > 0){
-                line.push(word.toString());
+                line.push(word.join(""));
                 word = [];
             }
         }
+
+        //Parse the line
+        this.parser.Parse(line);
+        return line.join();
+    }
+
+    Shell(){
+        const readline = require('readline-sync');
+        let exit : boolean = false;
+
+        const inputs : string[] = ["9+9","9 + 9"]; 
+
+        while(!exit){
+            const input = readline.question("> ");
+            this.source = input;
+            if(input === "exit"){
+                exit = true;
+                return;
+            }
+
+            while(!this.EOF){
+                this.Execute();
+            }
+            this.Reset();
+        }
+
+        // inputs.forEach(input => {
+        //     this.source = input;
+        //     console.log(this.Execute());
+        // });
     }
 }
